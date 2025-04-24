a8a46eaffa0d8d9c84594c3bd8db3c1bf6553493
rishiredekar <136117863+rishiredekar@users.noreply.github.com>
2025-04-24 11:40:15 +0000

    container div

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,18 @@
+// Primitive 
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100 
+const scoreValue = 100.5
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+// Non-Primitive / Reference Type
+// Array, Objects, Functions
