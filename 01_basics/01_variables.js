5a158117dd6873bc32d19933732998ec8c3bbe49
rishiredekar <136117863+rishiredekar@users.noreply.github.com>
2025-04-24 10:16:04 +0000

    container div

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "rishiredekar@gmail.com"
+var accountPassword = "67890"
+accountCity = "Thane"
+let accountState = "Maharashtra"
+
+// accountId = 2
+
+accountEmail = "rrr@gmail.com"
+accountPassword = "12345"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
