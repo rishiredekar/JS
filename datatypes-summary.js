7278c21e747d3900907fa4c3215f48a3034c5283
rishiredekar <136117863+rishiredekar@users.noreply.github.com>
2025-04-24 11:57:40 +0000

    container div

---
+++ datatypes-summary.js
@@ -0,0 +1,34 @@
+// Primitive 
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100 
+const scoreValue = 100.5
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+const bigNumber = 8788875865873456736n
+
+
+
+// Non-Primitive / Reference Type
+// Array, Objects, Functions
+
+const heros = ["hanunman", "naagraj", "shaktiman"]
+let myObj = {
+    name: "Rishi",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
