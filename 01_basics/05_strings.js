929594ac01ddb3fcef1d03d6bc34a362d0e04179
rishiredekar <136117863+rishiredekar@users.noreply.github.com>
2025-04-25 06:30:30 +0000

    container div

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,4 +5,30 @@ const repoCount = 24
 
 console.log(`Hello My Name Is ${name} And My Repo Count Is ${repoCount}`);
 
-const gameName = new String('rishiacc')
+const gameName = new String('rishi-acc')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('h'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "  rishi  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://rishi.com/rishi%20redekar"
+
+console.log(url.replace('%20', '_'));
+ 
+console.log(url.includes('rishi-redekar'));
+ 
+console.log(gameName.split('-'));
