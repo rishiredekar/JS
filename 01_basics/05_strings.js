c15d950aea611d96c41ded32c03028675af0e795
rishiredekar <136117863+rishiredekar@users.noreply.github.com>
2025-04-25 06:13:01 +0000

    container div

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,6 +1,8 @@
-const name = "rishi"
-const repoCount = 50
+const name = "Rishi"
+const repoCount = 24
 
 // console.log(name +  repoCount + " Value");
 
 console.log(`Hello My Name Is ${name} And My Repo Count Is ${repoCount}`);
+
+const gameName = new String('rishiacc')
